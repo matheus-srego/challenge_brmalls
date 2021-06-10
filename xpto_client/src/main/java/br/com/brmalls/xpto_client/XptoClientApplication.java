db6719502eb2f58d5658480b918dd9b8f97608be
Matheus da Silva Rego <matheus.gomes@al.infnet.edu.br>
2021-06-10 00:29:26 -0300

    Adding projectt

---
+++ xpto_client/src/main/java/br/com/brmalls/xpto_client/XptoClientApplication.java
@@ -0,0 +1,13 @@
+package br.com.brmalls.xpto_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XptoClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XptoClientApplication.class, args);
+	}
+
+}
---
+++ xpto_client/src/test/java/br/com/brmalls/xpto_client/XptoClientApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brmalls.xpto_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XptoClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
